f94f424e0455abe4b89cb34248bd1e0080c0df32
Francis <wayncis2@gmail.com>
2022-04-20 21:36:28 +0300

    Add comments to javascript code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const speakers = document.querySelector('.speakers');
 
-
 const members = [
   {
     mainDiv: ['speaker', 'flex-row', 'y-gap'],
@@ -76,41 +75,48 @@ const members = [
   }
 ];
 
-
 function createSpeaker(speaker){
+
+  // create speaker div
   const speakerDiv = document.createElement('div');
   speakerDiv.classList.add(...speaker.mainDiv)
 
+  // create speaker div
   const speakerImage = document.createElement('div');
   speakerImage.classList.add(speaker.imgDiv);
 
+  // create speaker image
   const img =  document.createElement('img');
   img.classList.add(speaker.imgAttributes[2]);
   img.src = speaker.imgAttributes[0];
   img.alt = speaker.imgAttributes[1];
   speakerImage.appendChild(img);
 
+  // create speaker boigraphy div
   const speakerBio = document.createElement('div');
   speakerBio.classList.add(speaker.bioDiv);
 
+  // create speaker name tag
   const h2 = document.createElement('h2');
   h2.textContent = speaker.head;
   speakerBio.appendChild(h2);
 
+  // create red paragraph
   const p1 = document.createElement('p');
   p1.classList.add(speaker.pAcademics);
   p1.textContent = speaker.pContent;
   speakerBio.appendChild(p1);
 
+  // create black paragraph
   const p2 = document.createElement('p');
   p2.classList.add(speaker.pRClass);
   p2.textContent = speaker.pRole;
   speakerBio.appendChild(p2);
 
+  // append the main divs to the page 
   speakerDiv.appendChild(speakerImage);
   speakerDiv.appendChild(speakerBio);
   speakers.appendChild(speakerDiv);
-
 }
 
 // create the speakers
